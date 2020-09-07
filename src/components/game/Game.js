a9f16f1fae0f3143f0001595d94f2af9f66f2d26
Bruno <brunochirelli@gmail.com>
2020-09-06 23:29:07 -0300

    change components to invert the order

--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,15 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/esm/Button";
+import Container from "react-bootstrap/esm/Container";
+import Row from "react-bootstrap/esm/Row";
+import Col from "react-bootstrap/esm/Col";
+
 import Attempt from "./Attempt";
+import Welcome from "./Welcome";
 
 const Game = () => {
   const [min, setMin] = useState(1);
   const [max, setMax] = useState(100);
   const [guess, setGuess] = useState(max / 2);
-
   const [attempts, setAttempts] = useState([]);
   const [attemptNumber, setAttemptNumber] = useState(1);
-
   const [stop, setStop] = useState(false);
 
   const handleReset = () => {
@@ -68,70 +71,78 @@ const Game = () => {
     }
   };
 
-  useEffect(() => {
-    //
-  }, []);
   return (
-    <>
-      {stop ? (
-        <div className="h4">
-          <p>Your number is {guess}!</p>
-          <p>
-            I found it in{" "}
-            <span style={{ color: "green" }}>{attemptNumber}</span> attempts.
-          </p>
-        </div>
-      ) : (
-        <div className="h5">Your number is {guess} ?</div>
-      )}
+    <Container style={{ maxWidth: "600px" }}>
+      <Row>
+        <Col>
+          <Welcome />
+        </Col>
+      </Row>
 
-      <div>
-        {stop ? (
-          <div>
-            <Button
-              value="higher"
-              onClick={handleReset}
-              style={{ margin: "1rem 0.75rem 1rem 0" }}
-            >
-              Reset
-            </Button>
-          </div>
-        ) : (
-          <div>
-            <Button
-              variant="primary"
-              value="higher"
-              style={{ margin: "1rem 0.75rem 1rem 0" }}
-              onClick={handleAnswer}
-            >
-              Higher
-            </Button>
-            <Button
-              variant="secondary"
-              value="lower"
-              style={{ margin: "1rem 0.75rem 1rem 0" }}
-              onClick={handleAnswer}
-            >
-              Lower
-            </Button>
-            <Button
-              variant="success"
-              value="right"
-              style={{ margin: "1rem 0.75rem 1rem 0" }}
-              onClick={handleAnswer}
-            >
-              Correct!
-            </Button>
-          </div>
-        )}
-      </div>
+      <Row>
+        <Col>
+          {stop ? (
+            <div className="h4">
+              <p>Your number is {guess}!</p>
+              <p>
+                I found it in{" "}
+                <span style={{ color: "green" }}>{attemptNumber}</span>{" "}
+                attempts.
+              </p>
+            </div>
+          ) : (
+            <div className="h5">Your number is {guess} ?</div>
+          )}
 
-      <div>
-        {attempts.map((e, i) => (
-          <div key={`attempt-${i}`}>{e}</div>
-        ))}
-      </div>
-    </>
+          <div>
+            {stop ? (
+              <div>
+                <Button
+                  value="higher"
+                  onClick={handleReset}
+                  style={{ margin: "1rem 0.75rem 1rem 0" }}
+                >
+                  Reset
+                </Button>
+              </div>
+            ) : (
+              <div>
+                <Button
+                  variant="primary"
+                  value="higher"
+                  style={{ margin: "1rem 0.75rem 1rem 0" }}
+                  onClick={handleAnswer}
+                >
+                  Higher
+                </Button>
+                <Button
+                  variant="secondary"
+                  value="lower"
+                  style={{ margin: "1rem 0.75rem 1rem 0" }}
+                  onClick={handleAnswer}
+                >
+                  Lower
+                </Button>
+                <Button
+                  variant="success"
+                  value="right"
+                  style={{ margin: "1rem 0.75rem 1rem 0" }}
+                  onClick={handleAnswer}
+                >
+                  Correct!
+                </Button>
+              </div>
+            )}
+          </div>
+
+          <div>
+            {attempts.map((e, i) => (
+              <div key={`attempt-${i}`}>{e}</div>
+            ))}
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- src/components/game/Welcome.js
+++ src/components/game/Welcome.js
@@ -1,31 +1,15 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-
-import Game from "./Game";
 
 const Welcome = () => {
   return (
-    <Container>
-      <Row>
-        <Col>
-          <h1>Let's play a game?</h1>
-          <p>
-            Let's play a game. I will guess your number from 1 to 100 in a
-            maximum of 7 attempts. Choose your number now. Only respond if your
-            number is <strong>higher</strong> or <strong>lower</strong> from my
-            guess.
-          </p>
-          <br></br>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Game />
-        </Col>
-      </Row>
-    </Container>
+    <main className="mt-4">
+      <h1>Guess, The Game</h1>
+      <p>
+        Let's play a game. I will guess your number from 1 to 100 in a maximum
+        of 7 attempts. Choose your number now. Only respond if your number is{" "}
+        <strong>higher</strong> or <strong>lower</strong> from my guess.
+      </p>
+    </main>
   );
 };
 
